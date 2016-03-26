d17fb6f4c6bb04b59a009a3bebff05febdd39b50
vikey89 <vincenzo.provenza89@gmail.com>
2016-03-26 15:08:58 +0100

    send email

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -17,6 +17,20 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+        $message = \Swift_Message::newInstance()
+            ->setSubject('Registrazione Confermata')
+            ->setFrom('info@myapp.com')
+            ->setTo('vincenzo.provenza89@gmail.com')
+            ->setBody(
+                $this->renderView(
+                    ':email:confirm-register.html.twig',
+                    ['name' => 'Mario Rossi']
+                ),
+                'text/html'
+            )
+        ;
+        $this->get('mailer')->send($message);
+
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),

f8abfb7a1392d6ace936f64a2dd6ade1e457190a
vikey89 <vincenzo.provenza89@gmail.com>
2016-03-26 16:29:43 +0100

    added translation

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -17,7 +17,8 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
+        $translated = $this->get('translator')->trans('Benvenuto');
+
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
         ]);

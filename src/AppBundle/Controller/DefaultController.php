a735c99ce526ff94230f51eea452f6ad2f924373
vikey89 <vincenzo.provenza89@gmail.com>
2016-04-01 14:14:47 +0200

    added response html

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -18,31 +18,11 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        //esempio di come recuperare l'url richiesto
-        $request->getPathInfo();
+        $response = new Response();
+        $response->setContent('<html><body><h1>Hello world!</h1></body></html>');
+        $response->setStatusCode(Response::HTTP_OK);
+        $response->headers->set('Content-Type', 'text/html');
 
-        //esempio di come recuperare un parametro dalla query string [GET]
-        $request->query->get('foo');
-
-        //esempio di come recuperare un parametro da POST
-        $request->request->get('bar', 'default value');
-
-        //esempio di come recuperare le variabili SERVER
-        $request->server->get('HTTP_HOST');
-
-        //esempio di come recuperare un'istanza di UploadedFile identificata come file
-        $request->files->get('file');
-
-        //esempio di come recuperare un parametro dal COOKIE
-        $request->cookies->get('PHPSESSID');
-
-        //esempio di come recuperare l'header
-        $request->headers->get('host');
-        $request->headers->get('content_type');
-
-        //esempio di come recuperare un array di lingue accettate dal client
-        $request->getLanguages();
-
-        return new Response();
+        return $response;
     }
 }

ed545e34cf77ce724b05a920335bb208e252d2ee
vikey89 <vincenzo.provenza89@gmail.com>
2016-04-01 14:11:22 +0200

    added example get params from request

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * Class DefaultController
@@ -17,9 +18,31 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
-        ]);
+        //esempio di come recuperare l'url richiesto
+        $request->getPathInfo();
+
+        //esempio di come recuperare un parametro dalla query string [GET]
+        $request->query->get('foo');
+
+        //esempio di come recuperare un parametro da POST
+        $request->request->get('bar', 'default value');
+
+        //esempio di come recuperare le variabili SERVER
+        $request->server->get('HTTP_HOST');
+
+        //esempio di come recuperare un'istanza di UploadedFile identificata come file
+        $request->files->get('file');
+
+        //esempio di come recuperare un parametro dal COOKIE
+        $request->cookies->get('PHPSESSID');
+
+        //esempio di come recuperare l'header
+        $request->headers->get('host');
+        $request->headers->get('content_type');
+
+        //esempio di come recuperare un array di lingue accettate dal client
+        $request->getLanguages();
+
+        return new Response();
     }
 }
